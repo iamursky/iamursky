18d423aa267aa84788a84fa3c47f3fc32ee75f6e
Ilya Amursky <ilya@amursky.com>
2026-03-06 18:03:13 +0100

    Fix Firebase initialization

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,21 +3,23 @@
 import type { FC, PropsWithChildren } from "react";
 
 import { getAnalytics, setAnalyticsCollectionEnabled } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
+import { getApp, initializeApp } from "firebase/app";
 import { initializePerformance } from "firebase/performance";
 import { useEffect } from "react";
 
+initializeApp({
+  apiKey: "AIzaSyAXSky1yVFcNwPsKQC_e_EPt7xaSCgWKBM",
+  appId: "1:862963964840:web:a83e075d6d4666da63db9d",
+  authDomain: "amursky-website.firebaseapp.com",
+  measurementId: "G-39KWYR5W15",
+  messagingSenderId: "862963964840",
+  projectId: "amursky-website",
+  storageBucket: "amursky-website.firebasestorage.app",
+});
+
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
   useEffect(() => {
-    const firebase = initializeApp({
-      apiKey: "AIzaSyAXSky1yVFcNwPsKQC_e_EPt7xaSCgWKBM",
-      appId: "1:862963964840:web:a83e075d6d4666da63db9d",
-      authDomain: "amursky-website.firebaseapp.com",
-      measurementId: "G-39KWYR5W15",
-      messagingSenderId: "862963964840",
-      projectId: "amursky-website",
-      storageBucket: "amursky-website.firebasestorage.app",
-    });
+    const firebase = getApp();
 
     const isProduction = process.env.NODE_ENV === "production";
 
